f172afd8014c8042e53604a2cd7be989eb4c9263
sahilalam0 <sahil.mansoor@anywhere.co>
2024-06-26 20:09:00 +0530

    refactor: customized theme for custom max width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "container-1/2": "calc(50% - 0.5rem)",
+        "container-1/3": "calc(33.333333% - 0.6666666667rem)",
+      },
+    },
   },
   plugins: [],
 };
